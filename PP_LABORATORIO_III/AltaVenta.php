3b47cd9afae83f79d377431214a8158697787239
Juan Sueldo <78236717+juansueldo@users.noreply.github.com>
2023-05-17 08:14:05 -0300

    Update AltaVenta.php

--- PP_LABORATORIO_III/AltaVenta.php
+++ PP_LABORATORIO_III/AltaVenta.php
@@ -13,10 +13,10 @@ echo agregarHamburguesa($arrayHamburguesas, $venta,$arrayCupones, $devolucionId)
 
 //Recibe por parametro un array de hamburguesas y un objeto de tipo venta  si existe el nombre y el tipo de hamburguesa
 //la venta se realiza, disminuyento el stock de dicha hamburguesa
-function agregarHamburguesa($arrayHamburguesas, $venta,$arrayCupones, $devolucionId)
+function agregarHamburguesa($arrayHamburguesas, $venta, $arrayCupones, $devolucionId)
 {
-    $arrayVentas = Archivos::LeerArchivoJSON("./Archivos/Venta.json");
-    $retorno = "No se vendio ninguna hamburguesa";
+    $arrayVentas = Archivos::LeerArchivoJSON("Venta.json");
+    $retorno = "No se vendió ninguna hamburguesa";
     $hamburguesaExistente = null;
     foreach ($arrayHamburguesas as &$hamburguesa) {
         if ($hamburguesa['nombre'] == $venta->nombreHamburguesa && $hamburguesa['tipo'] == $venta->tipoHamburguesa) {
@@ -25,23 +25,22 @@ function agregarHamburguesa($arrayHamburguesas, $venta,$arrayCupones, $devolucio
         }
     }
     if ($hamburguesaExistente != null) {
-        if($hamburguesaExistente['cantidad'] > 0 && $hamburguesaExistente['cantidad'] >= $venta->cantidadHamburguesa){
+        if ($hamburguesaExistente['cantidad'] > 0 && $hamburguesaExistente['cantidad'] >= $venta->cantidadHamburguesa) {
             $hamburguesaExistente['cantidad'] -= $venta->cantidadHamburguesa;
             $venta->precio = $hamburguesaExistente['precio'] * $venta->cantidadHamburguesa;
-            if(aplicarDescuento($arrayCupones,$devolucionId, $venta)){
-                $retorno = "Se vendio la hamburguesa: " . $hamburguesaExistente['nombre'] . ", del tipo: " . $hamburguesaExistente['tipo'] . "Con el descuento";
+            if (aplicarDescuento($arrayCupones, $devolucionId, $venta)) {
+                $retorno = "Se vendió la hamburguesa: " . $hamburguesaExistente['nombre'] . ", del tipo: " . $hamburguesaExistente['tipo'] . " con el descuento.";
+            } else {
+                $retorno = "Se vendió la hamburguesa: " . $hamburguesaExistente['nombre'] . ", del tipo: " . $hamburguesaExistente['tipo'];
             }
-            $retorno = "Se vendio la hamburguesa: " . $hamburguesaExistente['nombre'] . ", del tipo: " . $hamburguesaExistente['tipo'];
-            $venta->GuardarImagenVenta();
             $arrayVentas[] = $venta;
-            Archivos::GuardarObjetoJSON("./Archivos/Hamburguesa.json", $arrayHamburguesas);
-            Archivos::GuardarObjetoJSON("./Archivos/Venta.json", $arrayVentas);
-            Archivos::GuardarObjetoJSON("./Archivos/Cupones.json", $arrayCupones);
-        }
-        else{
+            Archivos::GuardarObjetoJSON("Hamburguesa.json", $arrayHamburguesas);
+            Archivos::GuardarObjetoJSON("Venta.json", $arrayVentas);
+            Archivos::GuardarObjetoJSON("Cupones.json", $arrayCupones);
+            return $retorno; // Se agrega un return aquí para salir de la función después de realizar la venta exitosamente
+        } else {
             $retorno = "No hay stock suficiente";
         }
-        
     }
 
     return $retorno;
